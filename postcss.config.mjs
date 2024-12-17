601c21db5df6982e455d82f863a95c6b94b1c820
jsh1147 <jsh1147@naver.com>
2024-12-18 06:57:35 +0900

    👷 chore: remove unnecessary code and files

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+const postConfig = {
   plugins: {
     tailwindcss: {},
   },
 };
 
-export default config;
+export default postConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,10 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const tailwindConfig = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
 } satisfies Config;
+
+export default tailwindConfig;
