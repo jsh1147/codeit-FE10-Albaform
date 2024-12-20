2b9c59cb20267620ced427269dcb98ccffc0c95f
heejin <77238424+devmanta@users.noreply.github.com>
2024-12-20 09:30:54 +0900

    👷 chore: add svgr config (#8)

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
